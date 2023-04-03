2d0a9ff6748d0287d2aaef20e9cda4dcf23d6444
chryschi <121822197+chryschi@users.noreply.github.com>
2023-04-03 21:24:33 +0200

    Display numbers on display if numbers are clicked

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ let a;
 let b;
 let operator;
 
+let displayValue;
+
+//Add EventListener to number buttons
+for (let i = 0; i < 10; i++) {
+  const numButton = document.querySelector(`#key${i}`);
+  numButton.addEventListener("click", () => {
+    displayValue = i;
+    display();
+  });
+}
+
+//Function for displaying stored value
+function display() {
+  const display = document.querySelector("#display");
+  display.textContent = displayValue;
+}
+
+//Operator function
 function operate(a, operator, b) {
   if (operator === "+") return add(a, b);
   else if (operator === "-") return subtract(a, b);
